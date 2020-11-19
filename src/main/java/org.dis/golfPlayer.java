8f6817a9eb01ba2b5ac0022993f7bcd522ae4e81
John Doe <johndoe@example.com>
2020-11-19 21:29:24 +0100

    Ultimo commit - resultado no del todo satisfactorio

--- src/main/java/org.dis/golfPlayer.java
+++ src/main/java/org.dis/golfPlayer.java
@@ -1,1 +1,16 @@
 //Clase para crear el objeto plano (POJO) con anotaciones
+
+    public class golfPlayer {
+        public string getPosicion() {
+            return posicion;
+        }
+
+        public void setPosicion(string posicion) {
+            this.posicion = posicion;
+        }
+
+        private string posicion;
+
+
+
+    }
